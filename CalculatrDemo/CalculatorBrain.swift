50a7b898e20f3b6a2e02024fb165cc8d8a292bfd
Andrew <anluanlu123@163.com>
2016-07-24 11:01:52 +0800

    commit

--- CalculatrDemo/CalculatorBrain.swift
+++ CalculatrDemo/CalculatorBrain.swift
@@ -9,44 +9,74 @@
 import UIKit
 func mutiply(op1:Double,op2:Double)->Double{
     
-    return 10
+    return op1*op2
 }
 
 
 class CalculatorBrain: NSObject {
-   
-        var accumlator:Double = 0
-        
-        var operations:Dictionary<String,Operation> = [
-            "1":Operation.Constant(M_PI),
-            "2":Operation.Constant(M_E),
-            "3":Operation.UnaryOperation(sqrt),
-            "cos":Operation.UnaryOperation(cos),
-            "x":Operation.BinaryOperation(mutiply)]
-        
-        enum Operation {
-            case Constant(Double)
-            case UnaryOperation((Double)->Double)
-            case BinaryOperation((Double,Double)->Double)
-            case Equals
-        }
-        
-        
-        func perfromOperation(symbol:String) -> Void {
-            if let operation = operations[symbol]{
-                switch operation {
-                case .Constant(let value):
-                    accumlator = value
-                case .UnaryOperation(let function):
-                    function(accumlator)
-                default:
-                    break
-                }
-            }
-        }
-        
-        
+    
+    var accumlator:Double = 0
+    
+    var operations:Dictionary<String,Operation> = [
+        "𝞹":Operation.Constant(M_PI),
+        "e":Operation.Constant(M_E),
+        "±":Operation.UnaryOperation({ -$0 }),
+        "√":Operation.UnaryOperation(sqrt),
+        "sin":Operation.UnaryOperation(sin),
+        "cos":Operation.UnaryOperation(cos),
+        "×":Operation.BinaryOperation({$0 * $1}),
+        "−":Operation.BinaryOperation({$0 - $1}),
+        "+":Operation.BinaryOperation({$0 + $1}),
+        "÷":Operation.BinaryOperation({$0 / $1}),
+        "=":Operation.Equals ]
+    
+    enum Operation {
+        case Constant(Double)
+        case UnaryOperation((Double)->Double)
+        case BinaryOperation((Double,Double)->Double)
+        case Equals
     }
     
+    
+    func perfromOperation(symbol:String) -> Void {
+        if let operation = operations[symbol]{
+            switch operation {
+            case .Constant(let value):
+                accumlator = value
+            case .UnaryOperation(let function):
+                accumlator=function(accumlator)
+            case .BinaryOperation(let foo):
+                executePedingOperation()
+                pending = PedingBinaryOperationInfo(binaryFunction: foo, firstOperand: accumlator)
+                print("pending.firstOperand:\(pending?.firstOperand)")
+            case .Equals:
+               executePedingOperation()
+            }
+        }
+    }
+    
+    func executePedingOperation() -> Void {
+        if pending != nil{
+            accumlator = pending!.binaryFunction(pending!.firstOperand, accumlator)
+        }
+    }
+    
+     var pending:PedingBinaryOperationInfo?
+    
+    struct PedingBinaryOperationInfo {
+        /// 声明一个函数
+        var binaryFunction : (Double,Double)->Double
+        var firstOperand:Double
+    }
+    
+    
+    var result:Double{
+        get{
+          return accumlator
+        }
+    }
+    
+}
+
 
 
--- CalculatrDemo/ViewController.swift
+++ CalculatrDemo/ViewController.swift
@@ -12,6 +12,8 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var showLb: UILabel!
     
+    var brain:CalculatorBrain = CalculatorBrain()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -21,12 +23,36 @@ class ViewController: UIViewController {
     @IBAction func numberBtnClick(sender: UIButton) {
         
         showLb.text = sender.currentTitle
+        brain.accumlator = Double(showLb.text!)!
     }
  
 
     @IBAction func clearAction(sender: UIButton) {
         showLb.text = ""
+        brain.accumlator = 0
+        brain.pending = nil
     }
 
+    /**
+     计算结果
+     
+     - parameter sender: <#sender description#>
+     */
+    @IBAction func caculatorAction(sender: UIButton) {
+       
+        brain.perfromOperation(sender.currentTitle!)
+        
+        let result = brain.result
+        
+        showLb.text = String(result)
+    }
 }
+
+
+
+
+
+
+
+
 
